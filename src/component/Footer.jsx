94a5ab44b499c3254ec65b19969ae0efd2149f75
kashafsaeed7842@gmail.com <kashafsaeed7842@gmail.com.noreply.github.com>
2026-02-10 14:35:06 +0500

    commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -18,16 +18,36 @@ function Footer() {
       </div>
 
       <div className='footer-bottom'>
-        <div className='footer-bottom-content-img'>
-          <img src="https://maree.qodeinteractive.com/wp-content/uploads/2019/05/landing-end-slider-img-1.png" alt="" className='leaf' />
-          <img src="https://maree.qodeinteractive.com/wp-content/uploads/2019/05/landing-end-slider-img-2-1.png" alt="" className='text' />
-          <img src="https://maree.qodeinteractive.com/wp-content/uploads/2019/04/short-slider-rev-1-img-3.png" alt="" className='boy' />
-          <img src="https://maree.qodeinteractive.com/wp-content/uploads/2019/05/landing-end-slider-img-5.png" alt="" className='bird' />
 
+        <div className='inner'>
+          <img src="https://maree.qodeinteractive.com/wp-content/uploads/2019/05/landing-end-slider-img-1.png" alt="" className='side-leaf' />
+        </div>
+        <div className='textimage'>
+          <div>
+            <img src="https://maree.qodeinteractive.com/wp-content/uploads/2019/05/landing-end-slider-img-3.png" alt="" className='bird' />
+          </div>
+          <img src="https://maree.qodeinteractive.com/wp-content/uploads/2019/05/landing-end-slider-img-2-1.png" alt="" className='text' />
+          <div>
+            <img src="\images\landing-slider-img-9.png" alt="" className='small' />
+          </div>
 
         </div>
-      
+        <div className='boy'>
+          <div>
+            <img src="https://maree.qodeinteractive.com/wp-content/uploads/2019/05/landing-end-slider-img-5.png" alt="" className='leaf' />
+
+          </div>
+          <img src="https://maree.qodeinteractive.com/wp-content/uploads/2019/04/short-slider-rev-1-img-3.png" alt="" className='boy' />
+
+        </div>
+        <div className='side'>
+          <img src="https://maree.qodeinteractive.com/wp-content/uploads/2019/05/landing-end-slider-img-7.png" alt="" className='side-leaf2' />
+
+        </div>
+
       </div>
+
+
 
 
 
